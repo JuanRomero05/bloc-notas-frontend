16fa3a513ee2d771f721a83cc60768b96708e718
Giovanni Urdaneta <117308178+giout@users.noreply.github.com>
2023-10-19 12:59:28 -0400

    Update Welcome.js
    persistencia de sesion implementada

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,28 +1,51 @@
 /* Es la primera screen que se muestra al entrar en la aplicación, en ella se muestra el Login */
+import { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import React from "react";
 import Login from "./Login";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Welcome = () => {
-
+     const [session, setSession] = useState(true)
      const navigation = useNavigation();
+
+     // al abrir la aplicacion, verifica si ya hay una sesion iniciada
+     useEffect(() => {
+          const verifyToken = async () => {
+               const token = await AsyncStorage.getItem('token')
+               if (token) {
+                    navigation.navigate('Menu')
+               } else {
+                    setSession(false)
+               }
+          }
+
+          verifyToken()
+     }, [])
+
      const handleSignup = () => {
           navigation.navigate("Signup")
      }
 
      return (
-          <View style={styles.container}>
-               <Image
-                    source={require("../assets/OIG.png")}
-                    style={styles.img}
-               />
-               <View>
-                    <Login />
-               </View>
-               <TouchableOpacity style={styles.buttonRegister} onPress={handleSignup}>
-                    <Text style={{ color: "#025099", fontSize: 16, fontWeight: "bold" }}>Registrarse</Text>
-               </TouchableOpacity>
+          <View>
+               {/*TODO: implementar loading donde dice null*/}
+               { session ? null : (
+                    <View style={styles.container}>
+                         <Image
+                              source={require("../assets/OIG.png")}
+                              style={styles.img}
+                         />
+                         <View>
+                              <Login />
+                         </View>
+                         <TouchableOpacity style={styles.buttonRegister} onPress={handleSignup}>
+                              <Text style={{ color: "#025099", fontSize: 16, fontWeight: "bold" }}>Registrarse</Text>
+                         </TouchableOpacity>
+                    </View>
+                    )
+               }
           </View>
      );
 };
